ec1f5efe6a9bcb2d3c91671ed5a0415cccd7b929
Zam <samuel.kodehode@gmail.com>
2024-03-01 14:10:24 +0100

    1.1.0

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const foodArray = [];
 const arrowKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+const fishRightImg = document.getElementById('fish-right');
+const fishLeftImg = document.getElementById('fish-left');
+const sharkLeftImg1 = document.getElementById('shark-left1');
+const sharkLeftImg2 = document.getElementById('shark-left2');
+const sharkLeftImg3 = document.getElementById('shark-left3');
+const sharkLeftImg4 = document.getElementById('shark-left4');
+const sharkRightImg1 = document.getElementById('shark-right1');
+const sharkRightImg2 = document.getElementById('shark-right2');
+const sharkRightImg3 = document.getElementById('shark-right3');
+const sharkRightImg4 = document.getElementById('shark-right4');
+let swimRightCounter = 1;
+let swimLeftCounter = 1;
+let lastKeyPress = 'left';
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let bacteria = {
@@ -16,6 +29,13 @@ let bacteria = {
         ArrowDown: false
     }
 };
+function drawImageOnCanvas(x, y, imageUrl, ctx) {
+    const img = new Image();
+    img.onload = function () {
+        ctx.drawImage(img, x, y, 20, 20);
+    };
+    img.src = imageUrl;
+}
 const getRandomNumberInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const drawRect = (x, y, w, h, color, ctx) => {
     ctx.fillStyle = color;
@@ -31,7 +51,7 @@ function drawCircle(ctx, x, y, radius, fillColor) {
 function calculateDistance(x1, y1, x2, y2) {
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 200; i++) {
     const x = getRandomNumberInRange(20, window.innerWidth - 20);
     const y = getRandomNumberInRange(20, window.innerHeight - 20);
     const velocityX = getRandomNumberInRange(-5, 5);
@@ -41,7 +61,7 @@ for (let i = 0; i < 300; i++) {
 }
 const gameLoop = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const speed = 2;
+    const speed = 5;
     if (bacteria.keyPressed.ArrowDown)
         bacteria.y += speed;
     if (bacteria.keyPressed.ArrowUp)
@@ -50,45 +70,51 @@ const gameLoop = () => {
         bacteria.x -= speed;
     if (bacteria.keyPressed.ArrowRight)
         bacteria.x += speed;
-    drawCircle(ctx, bacteria.x, bacteria.y, bacteria.size, 'blue');
+    if (bacteria.keyPressed.ArrowRight) {
+        lastKeyPress = 'right';
+        swimRightCounter += 6;
+        if (swimRightCounter <= 30) {
+            ctx.drawImage(sharkRightImg1, bacteria.x - (bacteria.size * 3.2 + 40) / 2, bacteria.y - (bacteria.size + 40) / 2, bacteria.size * 3.2 + 40, bacteria.size + 40);
+        }
+        else {
+            ctx.drawImage(sharkRightImg2, bacteria.x - (bacteria.size * 3.2 + 40) / 2, bacteria.y - (bacteria.size + 40) / 2, bacteria.size * 3.2 + 40, bacteria.size + 40);
+            if (swimRightCounter > 60)
+                swimRightCounter = 1;
+        }
+    }
+    else if (bacteria.keyPressed.ArrowLeft) {
+        lastKeyPress = 'left';
+        swimLeftCounter += 6;
+        if (swimLeftCounter <= 30) {
+            ctx.drawImage(sharkLeftImg1, bacteria.x - (bacteria.size * 3.2 + 40) / 2, bacteria.y - (bacteria.size + 40) / 2, bacteria.size * 3.2 + 40, bacteria.size + 40);
+        }
+        else {
+            ctx.drawImage(sharkLeftImg2, bacteria.x - (bacteria.size * 3.2 + 40) / 2, bacteria.y - (bacteria.size + 40) / 2, bacteria.size * 3.2 + 40, bacteria.size + 40);
+            if (swimLeftCounter > 60)
+                swimLeftCounter = 1;
+        }
+    }
+    else {
+        if (lastKeyPress === 'left') {
+            ctx.drawImage(sharkLeftImg2, bacteria.x - (bacteria.size * 3.2 + 40) / 2, bacteria.y - (bacteria.size + 40) / 2, bacteria.size * 3.2 + 40, bacteria.size + 40);
+        }
+        else {
+            ctx.drawImage(sharkRightImg2, bacteria.x - (bacteria.size * 3.2 + 40) / 2, bacteria.y - (bacteria.size + 40) / 2, bacteria.size * 3.2 + 40, bacteria.size + 40);
+        }
+    }
     foodArray.forEach((foodObj, index) => {
         const distance = calculateDistance(bacteria.x, bacteria.y, foodObj.x, foodObj.y);
         if (distance < bacteria.size) {
             foodArray.splice(index, 1);
             bacteria.size += 2 / 3.14;
         }
-        foodObj.velocityX += getRandomNumberInRange(-0.3, 0.3);
-        foodObj.velocityY += getRandomNumberInRange(-0.3, 0.3);
-        foodArray.forEach((obj) => {
-            const foodDistance = calculateDistance(foodObj.x, foodObj.y, obj.x, obj.y);
-            if (foodDistance <= 10) {
-                foodObj.y += foodObj.y < obj.y ? -1 : 1;
-            }
-            if (foodDistance <= 10) {
-                foodObj.x += foodObj.x > obj.x ? 1 : -1;
-            }
-            if (foodDistance < 50) {
-                foodObj.velocityY += obj.velocityY * 0.08;
-                foodObj.velocityX += obj.velocityX * 0.08;
-            }
-        });
+        foodObj.velocityX += getRandomNumberInRange(-0.2, 0.2);
+        foodObj.velocityY += getRandomNumberInRange(-0.1, 0.1);
         if (distance < 70) {
             foodObj.velocityX = foodObj.x > bacteria.x ? 5 : -5;
         }
         if (distance < 70) {
             foodObj.velocityY = foodObj.y > bacteria.y ? 5 : -5;
-        }
-        if (foodObj.velocityX > 4) {
-            foodObj.velocityX = 1;
-        }
-        else if (foodObj.velocityX < -5) {
-            foodObj.velocityX = -1;
-        }
-        if (foodObj.velocityY > 4) {
-            foodObj.velocityY = 1;
-        }
-        else if (foodObj.velocityY < -5) {
-            foodObj.velocityY = -1;
         }
         if (foodObj.x <= 200) {
             foodObj.velocityX += 0.9;
@@ -114,9 +140,39 @@ const gameLoop = () => {
         else if (foodObj.y >= window.innerHeight - 30) {
             foodObj.velocityY = -5;
         }
+        foodArray.forEach((obj) => {
+            const foodDistance = calculateDistance(foodObj.x, foodObj.y, obj.x, obj.y);
+            if (foodDistance <= 10) {
+                foodObj.y += foodObj.y < obj.y ? -0.5 : 0.5;
+            }
+            if (foodDistance <= 10) {
+                foodObj.x += foodObj.x > obj.x ? 0.5 : -0.5;
+            }
+            if (foodDistance < 50) {
+                foodObj.velocityY += obj.velocityY * 0.03;
+                foodObj.velocityX += obj.velocityX * 0.03;
+            }
+        });
+        if (foodObj.velocityX > 7) {
+            foodObj.velocityX = 1;
+        }
+        else if (foodObj.velocityX < -7) {
+            foodObj.velocityX = -1;
+        }
+        if (foodObj.velocityY > 4) {
+            foodObj.velocityY = 1;
+        }
+        else if (foodObj.velocityY < -5) {
+            foodObj.velocityY = -1;
+        }
         foodObj.x += foodObj.velocityX;
         foodObj.y += foodObj.velocityY;
-        drawRect(foodObj.x, foodObj.y, 7, 7, 'rgb(0,255,224)', ctx);
+        if (foodObj.velocityX <= 0) {
+            ctx.drawImage(fishLeftImg, foodObj.x, foodObj.y, 30, 30);
+        }
+        else {
+            ctx.drawImage(fishRightImg, foodObj.x, foodObj.y, 30, 30);
+        }
     });
     requestAnimationFrame(gameLoop);
 };
--- script.ts
+++ script.ts
@@ -2,6 +2,23 @@ const canvas = document.getElementById('canvas') as HTMLCanvasElement
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 const foodArray: Food[] = []
 const arrowKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
+const fishRightImg = document.getElementById('fish-right') as HTMLImageElement
+const fishLeftImg = document.getElementById('fish-left') as HTMLImageElement
+
+const sharkLeftImg1 = document.getElementById('shark-left1') as HTMLImageElement
+const sharkLeftImg2 = document.getElementById('shark-left2') as HTMLImageElement
+const sharkLeftImg3 = document.getElementById('shark-left3') as HTMLImageElement
+const sharkLeftImg4 = document.getElementById('shark-left4') as HTMLImageElement
+
+const sharkRightImg1 = document.getElementById('shark-right1') as HTMLImageElement
+const sharkRightImg2 = document.getElementById('shark-right2') as HTMLImageElement
+const sharkRightImg3 = document.getElementById('shark-right3') as HTMLImageElement
+const sharkRightImg4 = document.getElementById('shark-right4') as HTMLImageElement
+
+let swimRightCounter = 1
+let swimLeftCounter = 1
+
+let lastKeyPress = 'left'
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
@@ -30,6 +47,15 @@ let bacteria: Bacteria = {
 		ArrowDown: false
 	}
 }
+
+function drawImageOnCanvas(x, y, imageUrl, ctx) {
+	const img = new Image() // Create a new Image object
+	img.onload = function () {
+		// Draw the image on the canvas when it's loaded
+		ctx.drawImage(img, x, y, 20, 20) // Set desired dimensions (7x7) and position (x, y)
+	}
+	img.src = imageUrl // Set the source of the image
+}
 const getRandomNumberInRange = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min
 
 const drawRect = (x: number, y: number, w: number, h: number, color: string, ctx: CanvasRenderingContext2D): void => {
@@ -49,7 +75,7 @@ function calculateDistance(x1: number, y1: number, x2: number, y2: number): numb
 	return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 }
 
-for (let i = 0; i < 300; i++) {
+for (let i = 0; i < 200; i++) {
 	const x = getRandomNumberInRange(20, window.innerWidth - 20)
 	const y = getRandomNumberInRange(20, window.innerHeight - 20)
 	const velocityX = getRandomNumberInRange(-5, 5)
@@ -61,14 +87,74 @@ for (let i = 0; i < 300; i++) {
 const gameLoop = () => {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-	const speed = 2
+	const speed = 5
 
 	if (bacteria.keyPressed.ArrowDown) bacteria.y += speed
 	if (bacteria.keyPressed.ArrowUp) bacteria.y -= speed
 	if (bacteria.keyPressed.ArrowLeft) bacteria.x -= speed
 	if (bacteria.keyPressed.ArrowRight) bacteria.x += speed
 
-	drawCircle(ctx, bacteria.x, bacteria.y, bacteria.size, 'blue')
+	if (bacteria.keyPressed.ArrowRight) {
+		lastKeyPress = 'right'
+		swimRightCounter += 6
+		if (swimRightCounter <= 30) {
+			ctx.drawImage(
+				sharkRightImg1,
+				bacteria.x - (bacteria.size * 3.2 + 40) / 2,
+				bacteria.y - (bacteria.size + 40) / 2,
+				bacteria.size * 3.2 + 40,
+				bacteria.size + 40
+			)
+		} else {
+			ctx.drawImage(
+				sharkRightImg2,
+				bacteria.x - (bacteria.size * 3.2 + 40) / 2,
+				bacteria.y - (bacteria.size + 40) / 2,
+				bacteria.size * 3.2 + 40,
+				bacteria.size + 40
+			)
+			if (swimRightCounter > 60) swimRightCounter = 1
+		}
+	} else if (bacteria.keyPressed.ArrowLeft) {
+		lastKeyPress = 'left'
+		swimLeftCounter += 6
+		if (swimLeftCounter <= 30) {
+			ctx.drawImage(
+				sharkLeftImg1,
+				bacteria.x - (bacteria.size * 3.2 + 40) / 2,
+				bacteria.y - (bacteria.size + 40) / 2,
+				bacteria.size * 3.2 + 40,
+				bacteria.size + 40
+			)
+		} else {
+			ctx.drawImage(
+				sharkLeftImg2,
+				bacteria.x - (bacteria.size * 3.2 + 40) / 2,
+				bacteria.y - (bacteria.size + 40) / 2,
+				bacteria.size * 3.2 + 40,
+				bacteria.size + 40
+			)
+			if (swimLeftCounter > 60) swimLeftCounter = 1
+		}
+	} else {
+		if (lastKeyPress === 'left') {
+			ctx.drawImage(
+				sharkLeftImg2,
+				bacteria.x - (bacteria.size * 3.2 + 40) / 2,
+				bacteria.y - (bacteria.size + 40) / 2,
+				bacteria.size * 3.2 + 40,
+				bacteria.size + 40
+			)
+		} else {
+			ctx.drawImage(
+				sharkRightImg2,
+				bacteria.x - (bacteria.size * 3.2 + 40) / 2,
+				bacteria.y - (bacteria.size + 40) / 2,
+				bacteria.size * 3.2 + 40,
+				bacteria.size + 40
+			)
+		}
+	}
 
 	foodArray.forEach((foodObj: Food, index: number) => {
 		const distance = calculateDistance(bacteria.x, bacteria.y, foodObj.x, foodObj.y)
@@ -78,24 +164,8 @@ const gameLoop = () => {
 			bacteria.size += 2 / 3.14
 		}
 
-		foodObj.velocityX += getRandomNumberInRange(-0.3, 0.3)
-		foodObj.velocityY += getRandomNumberInRange(-0.3, 0.3)
-
-		foodArray.forEach((obj) => {
-			const foodDistance = calculateDistance(foodObj.x, foodObj.y, obj.x, obj.y)
-
-			if (foodDistance <= 10) {
-				foodObj.y += foodObj.y < obj.y ? -1 : 1
-			}
-			if (foodDistance <= 10) {
-				foodObj.x += foodObj.x > obj.x ? 1 : -1
-			}
-
-			if (foodDistance < 50) {
-				foodObj.velocityY += obj.velocityY * 0.08
-				foodObj.velocityX += obj.velocityX * 0.08
-			}
-		})
+		foodObj.velocityX += getRandomNumberInRange(-0.2, 0.2)
+		foodObj.velocityY += getRandomNumberInRange(-0.1, 0.1)
 
 		if (distance < 70) {
 			foodObj.velocityX = foodObj.x > bacteria.x ? 5 : -5
@@ -103,18 +173,6 @@ const gameLoop = () => {
 
 		if (distance < 70) {
 			foodObj.velocityY = foodObj.y > bacteria.y ? 5 : -5
-		}
-
-		if (foodObj.velocityX > 4) {
-			foodObj.velocityX = 1
-		} else if (foodObj.velocityX < -5) {
-			foodObj.velocityX = -1
-		}
-
-		if (foodObj.velocityY > 4) {
-			foodObj.velocityY = 1
-		} else if (foodObj.velocityY < -5) {
-			foodObj.velocityY = -1
 		}
 
 		if (foodObj.x <= 200) {
@@ -141,10 +199,45 @@ const gameLoop = () => {
 			foodObj.velocityY = -5
 		}
 
+		foodArray.forEach((obj) => {
+			const foodDistance = calculateDistance(foodObj.x, foodObj.y, obj.x, obj.y)
+
+			if (foodDistance <= 10) {
+				foodObj.y += foodObj.y < obj.y ? -0.5 : 0.5
+			}
+			if (foodDistance <= 10) {
+				foodObj.x += foodObj.x > obj.x ? 0.5 : -0.5
+			}
+
+			if (foodDistance < 50) {
+				foodObj.velocityY += obj.velocityY * 0.03
+				foodObj.velocityX += obj.velocityX * 0.03
+			}
+		})
+
+		if (foodObj.velocityX > 7) {
+			foodObj.velocityX = 1
+		} else if (foodObj.velocityX < -7) {
+			foodObj.velocityX = -1
+		}
+
+		if (foodObj.velocityY > 4) {
+			foodObj.velocityY = 1
+		} else if (foodObj.velocityY < -5) {
+			foodObj.velocityY = -1
+		}
+
 		foodObj.x += foodObj.velocityX
 		foodObj.y += foodObj.velocityY
 
-		drawRect(foodObj.x, foodObj.y, 7, 7, 'rgb(0,255,224)', ctx)
+		// Example usage
+		if (foodObj.velocityX <= 0) {
+			ctx.drawImage(fishLeftImg, foodObj.x, foodObj.y, 30, 30)
+		} else {
+			ctx.drawImage(fishRightImg, foodObj.x, foodObj.y, 30, 30)
+		}
+
+		/*	drawRect(foodObj.x, foodObj.y, 7, 7, 'rgb(0,255,224)', ctx)*/
 	})
 
 	requestAnimationFrame(gameLoop)
